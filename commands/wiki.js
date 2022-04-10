ffecca318b73a4d229e0ada5b090479c18cd390c
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-11 00:32:53 +0100

    add new subcommands

--- commands/wiki.js
+++ commands/wiki.js
@@ -18,7 +18,27 @@ module.exports = {
         .addSubcommand(subcommand =>
             subcommand
                 .setName('runes')
-                .setDescription('A link to the Runes and Bosses page!')),
+                .setDescription('A link to the Runes and Bosses page!'))
+
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('brewery')
+                .setDescription('A link to the Brewery page!'))
+
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('townyplugin')
+                .setDescription('A link to the Towny Plugin Guide!'))
+
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('siegewar')
+                .setDescription('A link to the Siege War Guide!'))
+
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('navalsiege')
+                .setDescription('A link to the Movecraft Naval Siege Guide!')),
 
 
         async execute(interaction) {
@@ -53,6 +73,46 @@ module.exports = {
         .setTimestamp() // Sets the current date and time at the bottom of the embed.
         .setFooter({ text: 'Bot written by Shadowevil015', iconURL: '' }) // Footer at the bottom of the embed.
 
+        const brewery = new MessageEmbed() // Create a message embed, called resourcepack.
+        .setColor('#0099ff') // Sets the sidebar colour of the embed.
+        .setTitle(bold('Brewery - Fermentation, Distillation, and more!')) // Sets the main title of the embed, in bold (who woulda guessed?)
+        .setDescription(italic('Use this article to learn more about Brewery!'))
+        .addFields(
+           { name: 'Link:', value: '[https://wiki.ccnetmc.com/en/Brewery](https://wiki.ccnetmc.com/en/Brewery)', inline: true },
+        )
+        .setTimestamp() // Sets the current date and time at the bottom of the embed.
+        .setFooter({ text: 'Bot written by Shadowevil015', iconURL: '' }) // Footer at the bottom of the embed.
+
+        const townyplugin = new MessageEmbed() // Create a message embed, called resourcepack.
+        .setColor('#0099ff') // Sets the sidebar colour of the embed.
+        .setTitle(bold('Towny Plugin Starter Guide')) // Sets the main title of the embed, in bold (who woulda guessed?)
+        .setDescription(italic('Use this article to learn more about the Towny plugin, used on both Nations and Towny!'))
+        .addFields(
+           { name: 'Link:', value: '[https://wiki.ccnetmc.com/en/Guides/TownyPlugin](https://wiki.ccnetmc.com/en/Guides/TownyPlugin)', inline: true },
+        )
+        .setTimestamp() // Sets the current date and time at the bottom of the embed.
+        .setFooter({ text: 'Bot written by Shadowevil015', iconURL: '' }) // Footer at the bottom of the embed.
+
+        const siegewar = new MessageEmbed() // Create a message embed, called resourcepack.
+        .setColor('#0099ff') // Sets the sidebar colour of the embed.
+        .setTitle(bold('Siege War Guide')) // Sets the main title of the embed, in bold (who woulda guessed?)
+        .setDescription(italic('Use this article to learn more about Sieges on Nations, and how they work!'))
+        .addFields(
+           { name: 'Link:', value: '[https://wiki.ccnetmc.com/en/Nations/WarSieging](https://wiki.ccnetmc.com/en/Nations/WarSieging)', inline: true },
+        )
+        .setTimestamp() // Sets the current date and time at the bottom of the embed.
+        .setFooter({ text: 'Bot written by Shadowevil015', iconURL: '' }) // Footer at the bottom of the embed.
+
+        const navalsiege = new MessageEmbed() // Create a message embed, called resourcepack.
+        .setColor('#0099ff') // Sets the sidebar colour of the embed.
+        .setTitle(bold('Movecraft Naval Siege Guide')) // Sets the main title of the embed, in bold (who woulda guessed?)
+        .setDescription(italic('Use this article to learn more about the Naval Sieges around the Nations map!'))
+        .addFields(
+           { name: 'Link:', value: '[https://wiki.ccnetmc.com/en/Nations/WarNavalSieges](https://wiki.ccnetmc.com/en/Nations/WarNavalSieges)', inline: true },
+        )
+        .setTimestamp() // Sets the current date and time at the bottom of the embed.
+        .setFooter({ text: 'Bot written by Shadowevil015', iconURL: '' }) // Footer at the bottom of the embed.
+
             if (interaction.options.getSubcommand() === "nationsguide") {
                 await interaction.reply( {embeds: [nationsguide]} )
             }
@@ -63,6 +123,22 @@ module.exports = {
 
             else if (interaction.options.getSubcommand() === "runes") {
                 await interaction.reply( {embeds: [runes]} )
+            }
+
+            else if (interaction.options.getSubcommand() === "brewery") {
+                await interaction.reply( {embeds: [brewery]} )
+            }
+
+            else if (interaction.options.getSubcommand() === "townyplugin") {
+                await interaction.reply( {embeds: [townyplugin]} )
+            }
+
+            else if (interaction.options.getSubcommand() === "siegewar") {
+                await interaction.reply( {embeds: [siegewar]} )
+            }
+
+            else if (interaction.options.getSubcommand() === "navalsiege") {
+                await interaction.reply( {embeds: [navalsiege]} )
             }
 	},
 };
