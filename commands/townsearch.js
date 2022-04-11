8934484b2f32e708c94012880951b7d113ac81cd
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-11 02:46:01 +0100

    update townsearch to add upkeep, bank, and what nation the town is in

--- commands/townsearch.js
+++ commands/townsearch.js
@@ -23,18 +23,28 @@ module.exports = {
 		var residents = JSON.stringify(complttown.residents) // Make a variable string for the residents of the town.
 		var mayor = JSON.stringify(complttown.mayor) // Make a variable string for the mayor of the town.
 		var coordinates = JSON.stringify("x: "+complttown.x+", "+"y: "+complttown.z) // Get both the x & y coordinate of the spawn point of the capital, & prefix with x/y.
+		var bank = JSON.stringify(complttown.bank)
+		var upkeep = JSON.stringify(complttown.upkeep)
+		var nation = JSON.stringify(complttown.nation)
+
+		var inNation = nation.replaceAll(/"/g, "").replaceAll("_", " ").replace(/(^\w|\s\w|\s\_)(\S*)/g, (_,m1,m2) => m1.toUpperCase()+m2.toLowerCase())
 
 		const town = new MessageEmbed() // Create a message embed, called capital.
 			.setColor('#0099ff') // Sets the sidebar colour of the embed.
-			.setTitle(bold(captownname)) // Sets the main title of the embed, in bold (who woulda guessed?)
-			.setDescription('Information about '+captownname) // Sets the description of the embed, in italics.
+			.setTitle(bold(`${captownname} (Member of ${inNation})`)) // Sets the main title of the embed, in bold (who woulda guessed?)
+			.setDescription(italic('Information about '+captownname)) // Sets the description of the embed, in italics.
 			.addFields(
 				{ name: '\u200B', value: '\u200B' }, // Blank row
 				{ name: 'Mayor:', value: mayor.replaceAll(/"/g, ""), inline: true }, // Inline title of mayor, and it displays the content of the var "mayor".
-				{ name: 'Coords:', value: coordinates.replaceAll(/"/g, ""), inline: true }, // Inline title of coordinates, and it displays the content of the var "coordinates".
+				{ name: 'Coords:', value: coordinates.replaceAll(/"/g, ""), inline: true },
 				{ name: '\u200B', value: '\u200B' }, // Blank row
 		)
-			.addField('Residents:', residents.replaceAll(/"/g, "").replaceAll(",", ", "), true) // Inline title of residents, and it removes the JSON symbols from the var "residents".
+			.addFields(
+				{ name: 'Bank:', value: bank.replaceAll(/"/g, ""), inline: true },
+				{ name: 'Upkeep:', value: upkeep.replaceAll(/"/g, ""), inline: true },
+				{ name: '\u200B', value: '\u200B' }, // Blank row
+		)
+			.addField('Residents:', residents.replaceAll(/"/g, "").replaceAll(",", ", ")) // Inline title of residents, and it removes the JSON symbols from the var "residents".
 			.addField('\u200B', '\u200B') // Blank row
 			.setTimestamp() // Sets the current date and time at the bottom of the embed.
 			.setFooter({ text: 'Bot written by Shadowevil015', iconURL: '' }); // Footer at the bottom of the embed.
