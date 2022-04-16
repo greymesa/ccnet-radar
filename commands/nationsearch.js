a5f879451ea57a425c07fcc6c2a3cb39d96ff901
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-16 18:17:36 +0100

    replaced the wrong var

--- commands/nationsearch.js
+++ commands/nationsearch.js
@@ -41,8 +41,8 @@ module.exports = {
                 { name: 'Capital:', value: capital.replaceAll(/"/g, ""), inline: true },
 				{ name: '\u200B', value: '\u200B' }, // Blank row
 		)
-            .addField('Towns:', first.replaceAll(/"/g, "").replaceAll(",", ", "))
-			.addField('Residents:', residents.replaceAll(/"/g, "").replaceAll(",", ", "))
+           // .addField('Towns:', .replaceAll(/"/g, "").replaceAll(",", ", "))
+			.addField('Residents:', first.replaceAll(/"/g, "").replaceAll(",", ", "))
 			.addField('\u200B', '\u200B')
 			.setTimestamp()
 			.setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
