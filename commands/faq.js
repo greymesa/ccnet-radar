5ed3dd63c326c857a688e346a3e11c4eaa4c5588
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-16 15:57:08 +0100

    add wiki subcmd

--- commands/faq.js
+++ commands/faq.js
@@ -8,8 +8,27 @@ module.exports = {
         .addSubcommand(subcommand =>
             subcommand
                 .setName('ip')
-                .setDescription('A list of the CCNetMC related IP Addresses')),
-
+                .setDescription('A list of the CCNetMC related IP Addresses!'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('wiki')
+                .setDescription('A link to our wiki!'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('resourcepack')
+                .setDescription('A link to the CCNet resourcepack!'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('map')
+                .setDescription('A link to the CCNet Dynmap!'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('logs')
+                .setDescription('A guide to submitting logs, for use in tickets!'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('reboot')
+                .setDescription('A guide on what to do, in the event of a server crash!')),
 
 async execute(interaction) {
 
@@ -26,12 +45,28 @@ async execute(interaction) {
                 { name: 'Asia:', value: inlineCode("asia.ccnetmc.com") },
                 { name: '\u200B', value: '\u200B' }, // Blank row
 		)
-                    .setTimestamp() // Sets the current date and time at the bottom of the embed.
-                    .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' }) // Footer at the bottom of the embed.
+                .setTimestamp() // Sets the current date and time at the bottom of the embed.
+                .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' }) // Footer at the bottom of the embed.
 
-        
+
+        const wiki = new MessageEmbed() // Create a message embed, called resourcepack.
+            .setColor('#EE6123') // Sets the sidebar colour of the embed.
+            .setTitle(bold('Wiki')) // Sets the main title of the embed, in bold (who woulda guessed?)
+            .setDescription(italic('You can view the wiki via this link:'))
+			.addFields(
+				{ name: '\u200B', value: '\u200B' }, // Blank row
+				{ name: 'Link:', value: '[https://www.dropbox.com/s/312j10m4cw611au/CCNet.zip?dl=1](https://www.dropbox.com/s/312j10m4cw611au/CCNet.zip?dl=1)' },
+                { name: '\u200B', value: '\u200B' }, // Blank row
+		)
+                .setTimestamp() // Sets the current date and time at the bottom of the embed.
+                .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' }) // Footer at the bottom of the embed.
+
          if (interaction.options.getSubcommand() === "ip") {
             await interaction.reply( {embeds: [ip]} )
+        }
+
+        else if (interaction.options.getSubcommand() === "wiki") {
+            await interaction.reply( {embeds: [wiki]} )
         }
     },
 };
