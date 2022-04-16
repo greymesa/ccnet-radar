9f8b1152d8ad4019cb42a92cd07dd44fcf4517a9
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-16 15:10:25 +0100

    actually make the embed

--- commands/faq.js
+++ commands/faq.js
@@ -13,7 +13,7 @@ module.exports = {
 
 async execute(interaction) {
 
-        const aircraftcarrier = new MessageEmbed() // Create a message embed, called resourcepack.
+        const ip = new MessageEmbed() // Create a message embed, called resourcepack.
             .setColor('#0099ff') // Sets the sidebar colour of the embed.
             .setTitle(bold('What is the IP?')) // Sets the main title of the embed, in bold (who woulda guessed?)
             .setDescription(italic('You can use the following IPs to join CCNet:'))
@@ -25,25 +25,12 @@ async execute(interaction) {
                 { name: '\u200B', value: '\u200B' }, // Blank row
                 { name: 'Asia:', value: "asia.ccnetmc.com" },
 		)
-                    .setImage('https://wiki.ccnetmc.com/nations/ships/aircraft_carrier.png')
                     .setTimestamp() // Sets the current date and time at the bottom of the embed.
                     .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' }) // Footer at the bottom of the embed.
 
         
-         if (interaction.options.getSubcommand() === "train") {
-            await interaction.reply( {embeds: [train]} )
-        }
-
-        else if (interaction.options.getSubcommand() === "truck") {
-            await interaction.reply( {embeds: [truck]} )
-        }
-
-        else if (interaction.options.getSubcommand() === "turret") {
-            await interaction.reply( {embeds: [turret]} )
-        }
-
-        else if (interaction.options.getSubcommand() === "zeppelin") {
-            await interaction.reply( {embeds: [zeppelin]} )
+         if (interaction.options.getSubcommand() === "ip") {
+            await interaction.reply( {embeds: [ip]} )
         }
     },
 };
