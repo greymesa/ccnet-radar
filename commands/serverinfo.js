fb21127bc6dc59d4deaaada584fefa008dc29419
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-23 17:47:35 +0100

    make test file for pinging servers

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -17,11 +17,6 @@ module.exports = {
         var hub = JSON.stringify(serverInfo.hub)
 		var firstServerStatus = JSON.stringify(serverInfo.serverOnline)
 
-		var MCAPI = require("minecraft-lib")
-
-		const serverData = await MCAPI.servers.get("play.ccnetmc.com:25567").catch(err => { return err })
-		var playerCount = serverData.players.online
-
 		let serverStatus;
 		if (firstServerStatus == "true"){
 			serverStatus = "Online"
@@ -40,7 +35,7 @@ module.exports = {
 				{ name: 'Nations:', value: nations.replaceAll(/"/g, ""), inline: true },
 				{ name: '\u200B', value: '\u200B' }, // Blank row
 		)
-			.addField('Movecraft:', playerCount)
+			//.addField('Movecraft:', playerCount)
 			.addFields(
 				{ name: 'Hub:', value: hub.replaceAll(/"/g, ""), inline: true },
 				{ name: 'Network:', value: network.replaceAll(/"/g, ""), inline: true },
