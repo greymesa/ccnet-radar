e11b758c81ad1c2f9dd7e87f84791f8fc51d89f8
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-25 00:45:49 +0100

    not even sure anymore

--- commands/onlinePlayers.js
+++ commands/onlinePlayers.js
@@ -12,7 +12,8 @@ module.exports = {
         var onlinePlayers = await fetch("https://shadowevil015.tech/api/v1/onlinePlayers").then(res => res.json()).catch(err => { return err })
     
         onlinePlayers.forEach((player) => {
-           const onlinePlayersName =  JSON.stringify(player.name);
+             const onlinePlayersName =  JSON.stringify(player.name);
+            return onlinePlayersName
         }
     )
 
@@ -24,7 +25,7 @@ module.exports = {
         .setColor('#EE6123') // Sets the sidebar colour of the embed.
         .setTitle(bold(`Online Players | ${nations}`)) // Sets the main title of the embed, in bold (who woulda guessed?)
         .addFields(
-            { name: 'Online Players:', value: JSON.stringify(player.name).replaceAll(/"/g, ""), inline: true },
+            { name: 'Online Players:', value: onlinePlayersName.replaceAll(/"/g, ""), inline: true },
     )
         .setTimestamp()
         .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
