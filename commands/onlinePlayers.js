1d76a25cd30b873510284db5e0815a613241eb87
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-25 00:42:26 +0100

    potential fix

--- commands/onlinePlayers.js
+++ commands/onlinePlayers.js
@@ -12,9 +12,7 @@ module.exports = {
         var onlinePlayers = await fetch("https://shadowevil015.tech/api/v1/onlinePlayers").then(res => res.json()).catch(err => { return err })
     
         onlinePlayers.forEach((player) => {
-           var onlinePlayersName =  JSON.stringify(player.name);
-
-           return onlinePlayersName
+           const onlinePlayersName =  JSON.stringify(player.name);
         }
     )
 
@@ -26,7 +24,7 @@ module.exports = {
         .setColor('#EE6123') // Sets the sidebar colour of the embed.
         .setTitle(bold(`Online Players | ${nations}`)) // Sets the main title of the embed, in bold (who woulda guessed?)
         .addFields(
-            { name: 'Online Players:', value: onlinePlayersName.replaceAll(/"/g, ""), inline: true },
+            { name: 'Online Players:', value: JSON.stringify(player.name).replaceAll(/"/g, ""), inline: true },
     )
         .setTimestamp()
         .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
--- test.js
+++ test.js
@@ -12,13 +12,19 @@ const fetch = require("node-fetch");
 async function getOnlinePlayers() {
 
     var onlinePlayers = await fetch("https://shadowevil015.tech/api/v1/onlinePlayers").then(res => res.json()).catch(err => { return err })
+
+    var testing = onlinePlayers.name
+
+    console.log(testing)
     
-    onlinePlayers.forEach((player) => {
+  /*  onlinePlayers.forEach((player) => {
        var stringy =  JSON.stringify(player.name);
        var counted = player.name.split(/"/g).length;
       // console.log(onlinePlayers.length);
        console.log(counted);
     }
-    )};
+)};*/ }
+
+
 
 getOnlinePlayers();
