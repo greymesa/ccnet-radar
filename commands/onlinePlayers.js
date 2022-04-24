a2427dc9c901529c7ab2c078bc25fa037606eec4
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-25 00:32:57 +0100

    onlinePlayers command

---
+++ commands/onlinePlayers.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder, bold, italic } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const fetch = require("node-fetch");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('onlineplayers')
+		.setDescription("Use this command to see a list of online players on Nations!"),
+
+	async execute(interaction) {
+
+        var onlinePlayers = await fetch("https://shadowevil015.tech/api/v1/onlinePlayers").then(res => res.json()).catch(err => { return err })
+    
+        onlinePlayers.forEach((player) => {
+           var onlinePlayersName =  JSON.stringify(player.name);
+
+        }
+    )
+
+    var serverInfo = await fetch("https://shadowevil015.tech/api/v1/serverInfo/").then(res => res.json()).catch(err => { return err })
+
+    var nations = JSON.stringify(serverInfo.nations)
+
+        const onlineplayers = new MessageEmbed() // Create a message embed
+        .setColor('#EE6123') // Sets the sidebar colour of the embed.
+        .setTitle(bold(`Online Players | ${nations}`)) // Sets the main title of the embed, in bold (who woulda guessed?)
+        .addFields(
+            { name: 'Online Players:', value: onlinePlayersName.replaceAll(/"/g, ""), inline: true },
+    )
+        .setTimestamp()
+        .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
+
+		await interaction.reply({ embeds: [onlineplayers] });
+        
+	},
+};
--- test.js
+++ test.js
@@ -14,9 +14,10 @@ async function getOnlinePlayers() {
     var onlinePlayers = await fetch("https://shadowevil015.tech/api/v1/onlinePlayers").then(res => res.json()).catch(err => { return err })
     
     onlinePlayers.forEach((player) => {
-       // console.log(player);
        var stringy =  JSON.stringify(player.name);
-       console.log(stringy);
+       var counted = player.name.split(/"/g).length;
+      // console.log(onlinePlayers.length);
+       console.log(counted);
     }
     )};
 
