38674daa33ee0ced28b9f915f0ad523eceebccd7
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-30 15:49:32 +0100

    remove await for the timeout

--- commands/fullMap.js
+++ commands/fullMap.js
@@ -20,7 +20,7 @@ module.exports = {
         .then(async (browser) => {
             const page = await browser.newPage();
             await page.goto("https://map.ccnetmc.com/nationsmap/#world;flat;0,64,0;0"); // Define the url
-            await setTimeout(func, 3000)
+            setTimeout(func, 9000)
             await page.screenshot({ path: "map.png" }); // Screenshot and save the file as map.png. The path can be configured
             await browser.close(); // Close the headless browser
     });
