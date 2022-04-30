35769e5e002e77ea59973b361da1ed9e300e02e2
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-30 16:51:35 +0100

    format the code a bit

--- commands/fullMap.js
+++ commands/fullMap.js
@@ -8,30 +8,38 @@ module.exports = {
 
 	async execute(interaction) {
 
-    const puppeteer = require("puppeteer");
-        puppeteer
-         .launch({ 
-          defaultViewport: {    // Define the size of the screenshot/headless browser
-            width: 1920,
-            height: 1080,
-        },
-    })
-        .then(async (browser) => {
-            const page = await browser.newPage();
-            await page.goto("https://map.ccnetmc.com/nationsmap/#world;flat;0,64,0;0"); // Define the url
-            await page.waitFor(15000)
-            await page.screenshot({ path: "map.png" }); // Screenshot and save the file as map.png. The path can be configured
-            await browser.close(); // Close the headless browser
-    });
+		const puppeteer = require("puppeteer");
+		puppeteer
+			.launch({
+				defaultViewport: { // Define the size of the screenshot/headless browser
+					width: 1920,
+					height: 1080,
+				},
+			})
+			.then(async (browser) => {
+				const page = await browser.newPage();
+				await page.goto("https://map.ccnetmc.com/nationsmap/#world;flat;0,64,0;0"); // Define the url
+				await page.waitFor(15000)
+				await page.screenshot({
+					path: "map.png"
+				}); // Screenshot and save the file as map.png. The path can be configured
+				await browser.close(); // Close the headless browser
+			});
 
-        const map = new MessageEmbed()
-        .setColor('#EE6123')
-        .setTitle(bold(`Nations Map`))
-        .setTimestamp()
-        .setImage('attachment://map.png')
-        .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
+		const map = new MessageEmbed()
+			.setColor('#EE6123')
+			.setTitle(bold(`Nations Map`))
+			.setTimestamp()
+			.setImage('attachment://map.png')
+			.setFooter({
+				text: 'Bot written by Shadowevil015',
+				iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437'
+			});
 
-            await interaction.reply({ embeds: [map], files: ['images/map.png'] });
+		await interaction.reply({
+			embeds: [map],
+			files: ['images/map.png']
+		});
 
-    },
+	},
 };
