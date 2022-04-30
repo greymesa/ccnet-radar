8bc1ff6978337f4195a1d0ed0dfd69a629dca7d5
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-30 23:52:17 +0100

    foundations of the siege command

--- commands/sieges.js
+++ commands/sieges.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, bold } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const fetch = require("node-fetch");
 
@@ -9,9 +9,27 @@ module.exports = {
 
 	async execute(interaction) {
 
-        var sieges = await fetch('https://shadowevil015.tech/api/v1/sieges')
+        var sieges = await fetch('https://shadowevil015.tech/api/v1/sieges').then(res => res.json()).catch(err => { return err })
+		
+		var siegeList = []
 
-    await interaction.reply({ sieges/*embeds: [onlineplayers]*/ });
+        sieges.forEach((siege) => {
+            siegeList.push(siege.name)
+        }
+    )
+
+		var strsieges = JSON.stringify(siegeList)
+
+		const siege = new MessageEmbed()
+			.setColor('#EE6123')
+			.setTitle(bold(`Current Sieges`))
+			.addFields(
+				{ name: '\u200B', value: strsieges.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
+			)
+			.setTimestamp()
+			.setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
+
+    await interaction.reply({ embeds: [siege] });
         
 	},
 };
