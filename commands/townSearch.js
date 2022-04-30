24655858e4b864c312508b48758cfe8ce51e2eb2
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-04-30 19:14:36 +0100

    make the "gimme a sec" visable by only the cmd issuer

--- commands/townSearch.js
+++ commands/townSearch.js
@@ -85,9 +85,9 @@ module.exports = {
 			.setTimestamp()
 			.setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
 
-			await interaction.reply(`gimme a sec`)
+			await interaction.reply({ content: `gimme a sec`, ephemeral: true });
 			
-			await wait(7500)
+			await wait(7500);
 
 			await interaction.editReply({ embeds: [town], files: [`images/${townname}.png`] } );
 	},
