6445206d8a0deb7636f0a78720d4dfccef938584
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-05-01 15:08:46 +0100

    add siege time

--- commands/sieges.js
+++ commands/sieges.js
@@ -18,13 +18,22 @@ module.exports = {
         }
     )
 
+		var siegeTimeList = []
+
+	    sieges.forEach((time) => {
+            siegeTimeList.push(time.time)
+        }
+    )
+
 		var strsieges = JSON.stringify(siegeList)
+		var strtimes = JSON.stringify(siegeTimeList)
 
 		const siege = new MessageEmbed()
 			.setColor('#EE6123')
 			.setTitle(bold(`Current Sieges`))
 			.addFields(
 				{ name: '\u200B', value: strsieges.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
+				{ name: '\u200B', value: strtimes.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
 			)
 			.setTimestamp()
 			.setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
