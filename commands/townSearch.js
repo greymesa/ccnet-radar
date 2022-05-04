d98fb9f6f56d368348ef96402cd8c8bd94dfd392
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-05-04 12:31:00 +0100

    simple if/else to see if there are any online residents

--- commands/townSearch.js
+++ commands/townSearch.js
@@ -85,11 +85,15 @@ module.exports = {
       .addField(
         "Residents:", codeBlock(fn.removeStyleCharacters(residents.replaceAll(",", ", ")))
       )
-      .addField(
-        "Online Residents:", codeBlock(fn.removeStyleCharacters(strOnlineResidents.replaceAll(",", ", ")))
-      )
       .setTimestamp()
       .setFooter({text: "Bot written by Shadowevil015", iconURL:"https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437",})};
+
+      if (onlineResidents.length == 0) {
+        town.addField("Online Residents:", codeBlock("There are no online residents in this town!"));
+      }
+      else {
+        town.addField("Online Residents:", codeBlock(fn.removeStyleCharacters(strOnlineResidents.replaceAll(",", ", "))));
+      }
 
       if (complttown === "That town does not exist!") {
         await interaction.reply("I can't find a town with that name! Did you spell it correctly?");
