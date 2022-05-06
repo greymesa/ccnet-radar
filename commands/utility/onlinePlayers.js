1fffc52726d932ce0588573969da26ec5870ef29
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-05-06 20:03:52 +0100

    use if/else to remove empty fields from /onlineplayers

--- commands/utility/onlinePlayers.js
+++ commands/utility/onlinePlayers.js
@@ -12,7 +12,10 @@ module.exports = {
 
         var onlinePlayers = await fetch("https://shadowevil015.tech/api/v1/onlinePlayers/").then(res => res.json()).catch(err => { return err })
 
-     var onlinePlayersName = []
+
+    const onlineplayers = new MessageEmbed()
+
+    var onlinePlayersName = []
 
         onlinePlayers.forEach((player) => {
             onlinePlayersName.push(player.name)
@@ -25,30 +28,43 @@ module.exports = {
 
     var serverInfo = await fetch("https://shadowevil015.tech/api/v1/serverInfo/").then(res => res.json()).catch(err => { return err })
 
-    var nations = JSON.stringify(serverInfo.nations)
+    const nations = JSON.stringify(serverInfo.nations)
 
     const players1 = []
-    players1.push(sortedPlayers.slice(0, 37))
+
+    if (nations >= 1) {
+        players1.push(sortedPlayers.slice(0, 37))
+        let strp1 = JSON.stringify(players1)
+        onlineplayers.addField( 'Online Players:', inlineCode(strp1).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replaceAll("[", ""), true )}
 
     const players2 = []
-    players2.push(sortedPlayers.slice(38, 75))
+
+    if (nations >= 36) {
+        players2.push(sortedPlayers.slice(38, 75))
+        let strp2 = JSON.stringify(players2)
+        onlineplayers.addField( '\u200B', inlineCode(strp2).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replaceAll("[", ""), true )}
+    else {}
 
     const players3 = []
     
-    players3.push(sortedPlayers.slice(76, 113))
+    if (nations >= 75) {
+        players3.push(sortedPlayers.slice(76, 113))
+        let strp3 = JSON.stringify(players3)
+        onlineplayers.addField( '\u200B', inlineCode(strp3).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replaceAll("[", ""), true )}
 
-       const onlineplayers = new MessageEmbed() // Create a message embed
-        .setColor('#EE6123') // Sets the sidebar colour of the embed.
+    else {}
+
+        onlineplayers.setColor('#EE6123') // Sets the sidebar colour of the embed.
         .setTitle(bold(`Online Players | ${nations}`)) // Sets the main title of the embed, in bold (who woulda guessed?)
-        .addFields(
-            { name: 'Online Players:', value: inlineCode(players1).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replace("[", ""), inline: true },
-            { name: '\u200B', value: inlineCode(players2).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replace("[", ""), inline: true },
-            { name: '\u200B', value: inlineCode(players3).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replace("[", ""), inline: true },
-    )
         .setTimestamp()
         .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
 
-		await interaction.reply({ embeds: [onlineplayers] });
+
+        if (nations >= 1) {
+		    await interaction.reply( { embeds: [onlineplayers] } )}
+        else {
+            await interaction.reply( "No players are online! Is the server down?" )
+        }
         
 	},
 };
