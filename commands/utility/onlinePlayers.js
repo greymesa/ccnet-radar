9122e5771c7787f59508fb1e6437e9642a545a13
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-05-08 18:20:15 +0100

    revert onlineplayers to pre-PR state - otherwise it does not work

--- commands/utility/onlinePlayers.js
+++ commands/utility/onlinePlayers.js
@@ -9,50 +9,60 @@ module.exports = {
 
 	async execute(interaction) {
 
-        const onlineplayers = new MessageEmbed()
-        
-        var onlinePlayers = await fetch("https://shadowevil015.tech/api/v1/onlinePlayers/").then(res => res.json()).catch(err => { return err });
+    let onlinePlayers = await fetch("https://shadowevil015.tech/api/v1/onlinePlayers/").then(res => res.json()).catch(err => { return err })
 
-        let onlinePlayersName = [];
+    const onlineplayers = new MessageEmbed()
+
+    var onlinePlayersName = []
+
         onlinePlayers.forEach((player) => {
-            onlinePlayersName.push(player.name);
-        });
+            onlinePlayersName.push(player.name)
+        }
+    )
 
-        let sortedPlayers = onlinePlayersName.sort(function (a, b) {
+    let sortedPlayers = onlinePlayersName.sort(function (a, b) {
             return a.toLowerCase().localeCompare(b.toLowerCase());
         });
 
-        const serverInfo = await fetch("https://shadowevil015.tech/api/v1/serverInfo/").then(res => res.json()).catch(err => { return err });
+    let serverInfo = await fetch("https://shadowevil015.tech/api/v1/serverInfo/").then(res => res.json()).catch(err => { return err })
 
-        const nations = JSON.stringify(serverInfo.nations);
+    const nations = JSON.stringify(serverInfo.nations)
 
-        let players1 = [];
-        let players2 = [];
-        let players3 = [];
+    const players1 = []
+
+    if (nations >= 1) {
+        players1.push(sortedPlayers.slice(0, 37))
+        let strp1 = JSON.stringify(players1)
+        onlineplayers.addField( 'Online Players:', inlineCode(strp1).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replaceAll("[", ""), true )}
+
+    const players2 = []
+
+    if (nations >= 36) {
+        players2.push(sortedPlayers.slice(38, 75))
+        let strp2 = JSON.stringify(players2)
+        onlineplayers.addField( '\u200B', inlineCode(strp2).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replaceAll("[", ""), true )}
+    else {}
+
+    const players3 = []
+    
+    if (nations >= 75) {
+        players3.push(sortedPlayers.slice(76, 113))
+        let strp3 = JSON.stringify(players3)
+        onlineplayers.addField( '\u200B', inlineCode(strp3).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replaceAll("[", ""), true )}
+
+    else {}
+
+        onlineplayers.setColor('#EE6123') // Sets the sidebar colour of the embed.
+        .setTitle(bold(`Online Players | ${nations}`)) // Sets the main title of the embed, in bold (who woulda guessed?)
+        .setTimestamp()
+        .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
+
 
         if (nations >= 1) {
-            players1.push(sortedPlayers.slice(0, 37));
-            const strp1 = JSON.stringify(players1);
-            onlineplayers.addField('Online Players:', inlineCode(strp1).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replaceAll("[", ""), true);
-        } else if (nations >= 36) {
-            players2.push(sortedPlayers.slice(38, 75));
-            const strp2 = JSON.stringify(players2);
-            onlineplayers.addField('\u200B', inlineCode(strp2).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replaceAll("[", ""), true);
-        } else if (nations >= 75) {
-            players3.push(sortedPlayers.slice(76, 113));
-            const strp3 = JSON.stringify(players3);
-            onlineplayers.addField('\u200B', inlineCode(strp3).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replaceAll("[", ""), true);
+		    await interaction.reply( { embeds: [onlineplayers] } )}
+        else {
+            await interaction.reply( "No players are online! Is the server down?" )
         }
-
-            onlineplayers.setColor('#EE6123') // Sets the sidebar colour of the embed.
-            .setTitle(bold(`Online Players | ${nations}`)) // Sets the main title of the embed, in bold (who woulda guessed?)
-            .setTimestamp()
-            .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
-
-        if (nations >= 1) {
-	        await interaction.reply({ embeds: [onlineplayers] });
-        } else {
-            await interaction.reply("No players are online! Is the server down?");
-        }
+        
 	},
 };
