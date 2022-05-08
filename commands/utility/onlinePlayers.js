fd8a4277b7e440fb207e3b6d002f292c3549bba3
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-05-08 18:13:18 +0100

    fix for onlineplayers being used before initialising

--- commands/utility/onlinePlayers.js
+++ commands/utility/onlinePlayers.js
@@ -8,7 +8,10 @@ module.exports = {
 		.setDescription("Use this command to see a list of online players on Nations!"),
 
 	async execute(interaction) {
-        const onlinePlayers = await fetch("https://shadowevil015.tech/api/v1/onlinePlayers/").then(res => res.json()).catch(err => { return err });
+
+        const onlineplayers = new MessageEmbed()
+        
+        var onlinePlayers = await fetch("https://shadowevil015.tech/api/v1/onlinePlayers/").then(res => res.json()).catch(err => { return err });
 
         let onlinePlayersName = [];
         onlinePlayers.forEach((player) => {
@@ -41,8 +44,7 @@ module.exports = {
             onlineplayers.addField('\u200B', inlineCode(strp3).replaceAll(/"/g, "").replaceAll(/,/g, "\n").replace(/]/g, "").replaceAll("[", ""), true);
         }
 
-        const onlineplayers = new MessageEmbed()
-            .setColor('#EE6123') // Sets the sidebar colour of the embed.
+            onlineplayers.setColor('#EE6123') // Sets the sidebar colour of the embed.
             .setTitle(bold(`Online Players | ${nations}`)) // Sets the main title of the embed, in bold (who woulda guessed?)
             .setTimestamp()
             .setFooter({ text: 'Bot written by Shadowevil015', iconURL: 'https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437' });
