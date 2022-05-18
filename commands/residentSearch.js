da61bb10753e7d1e4236b58416de63be30ee6f3b
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-05-18 21:07:02 +0100

    online/offline detector for resident search

--- commands/residentSearch.js
+++ commands/residentSearch.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, bold, inlineCode } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
 const ccnet = require("ccnetmc");
+const fetch = require("node-fetch");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -16,38 +17,73 @@ module.exports = {
     ),
 
   async execute(interaction) {
+    
+    await interaction.deferReply()
+
+      let onlinePlayers = await ccnet.getOnlinePlayers().then(players => { return players });
+  
+      var onlinePlayersName = []
+  
+          onlinePlayers.forEach((player) => {
+              onlinePlayersName.push(player.name)
+          }
+      )
+  
+      let sortedPlayers = onlinePlayersName.sort(function (a, b) {
+              return a.toLowerCase().localeCompare(b.toLowerCase());
+          });
+
     const username = interaction.options.getString("name");
+
+    const resident = new MessageEmbed()
 
     const compltusername = await ccnet.getPlayer(username).then(resident => { return resident });
 
     if (compltusername === "That player does not exist!") {
       await interaction.reply("I can't find this player! Are you sure you spelt their name correctly?")}
     else {
+      const strSortedPlayers = JSON.stringify(sortedPlayers);
       const town = JSON.stringify(compltusername.town);
       const nation = JSON.stringify(compltusername.nation);
       const endpointName = JSON.stringify(compltusername.name);
+      const nickname = JSON.stringify(compltusername.nickname);
       const capitalisedName = endpointName.replaceAll(/"/g, "");
       const rank = JSON.stringify(compltusername.rank);
       const actualRank = rank.replaceAll(/"/g, "");
+      
+      let status;
+      if (strSortedPlayers.includes(endpointName)) {
+        status = "Online";
+        resident.setColor("GREEN")
+      } else {
+        status = "Offline";
+        resident.setColor("RED")
+      }
 
       let codedName;
       if (capitalisedName.includes("_")) {
         codedName = inlineCode(capitalisedName);
       } else {
         codedName = capitalisedName;
-      }
 
-      const resident = new MessageEmbed()
-        .setColor("#EE6123")
-        .setTitle(bold(`${codedName} | ${actualRank}`))
+        resident.setTitle(bold(`${codedName} | ${status}`))
         .addFields(
           { name: "Town:", value: town.replaceAll(/"/g, "").replaceAll(/_/g, " "), inline: true},
-          { name: "Nation:", value: nation.replaceAll(/"/g, "").replaceAll(/_/g, " "), inline: true }
+          { name: "Nation:", value: nation.replaceAll(/"/g, "").replaceAll(/_/g, " "), inline: true },
+          { name: "\u200B", value: "\u200B", inline: true }
         )
         .setTimestamp()
         .setFooter({ text: "Bot written by Shadowevil015", iconURL:"https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437"});
 
-      await interaction.reply({ embeds: [resident] })
+      if (status === "Online") {
+        const inGameRank = nickname.split("]")[0].replace("[", "").replaceAll(/"/g, "");
+        resident.addField("Rank:", actualRank, true)
+        resident.addField("In-game Rank:", inGameRank, true)
+        resident.addField("\u200B", "\u200B", true)
+      } else {}
+
+          await interaction.editReply(({ embeds: [resident] }))
+      }
     }
   }
 }
