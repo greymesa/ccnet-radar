9c7f52edf06d1382aa79bcd5f0cf4e3c44f88002
Shadowevil015 <brandonbevan.2004@gmail.com>
2022-10-23 21:39:17 +0100

    kinda improve siege list idk

--- commands/sieges.js
+++ commands/sieges.js
@@ -29,27 +29,105 @@ module.exports = {
       siegeList.push(siege.name);
       siegeTimeList.push(siege.time);
       siegeBalanceList.push(siege.points);
+
     });
 
-    const strsieges = JSON.stringify(siegeList);
-    const strtimes = JSON.stringify(siegeTimeList);
-    const strbalances = JSON.stringify(siegeBalanceList);
+    let siege1
+    let siege2
+    let siege3
+    let siege4
+    let siege5
+    let siege6
+
+  if (siegeList.length != 0) {
+   siege1 = {
+      name: siegeList[0].replace(" ", "\n"),
+      time: `Time Left: ${siegeTimeList[0]}`,
+      balance: `Points: ${siegeBalanceList[0]}`,
+      winner: siegeBalanceList[0].includes("-") ? "Defender Lead" : "Attacker Lead"
+    }}
+  if (siegeList[1] != undefined) {
+   siege2 = {
+      name: siegeList[1].replace(" ", "\n"),
+      time: `Time Left: ${siegeTimeList[1]}`,
+      balance: `Points: ${siegeBalanceList[1]}`,
+      winner: siegeBalanceList[1].includes("-") ? "Defender Lead" : "Attacker Lead"
+    }}
+
+  if (siegeList[2] != undefined) {
+   siege3 = {
+      name: siegeList[2].replace(" ", "\n"),
+      time: `Time Left: ${siegeTimeList[2]}`,
+      balance: `Points: ${siegeBalanceList[2]}`,
+      winner: siegeBalanceList[2].includes("-") ? "Defender Lead" : "Attacker Lead"
+    }}
+  if (siegeList[3] != undefined) {
+    let siege4 = {
+      name: siegeList[3].replace(" ", "\n"),
+      time: `Time Left: ${siegeTimeList[3]}`,
+      balance: `Points: ${siegeBalanceList[3]}`,
+      winner: siegeBalanceList[3].includes("-") ? "Defender Lead" : "Attacker Lead"
+    }}
+  if (siegeList[4] != undefined) {
+    let siege5 = {
+      name: siegeList[4].replace(" ", "\n"),
+      time: `Time Left: ${siegeTimeList[4]}`,
+      balance: `Points: ${siegeBalanceList[4]}`,
+      winner: siegeBalanceList[4].includes("-") ? "Defender Lead" : "Attacker Lead"
+    }}
+  if (siegeList[5] != undefined) {
+    let siege6 = {
+      name: siegeList[5].replace(" ", "\n"),
+      time: `Time Left: ${siegeTimeList[5]}`,
+      balance: `Points: ${siegeBalanceList[5]}`,
+      winner: siegeBalanceList[5].includes("-") ? "Defender Lead" : "Attacker Lead"
+    }}
+
+    const embedSiege = new MessageEmbed()
+    .setColor("#EE6123")
+    .setTitle(bold(`Current Sieges`))
+    .addFields(
+      { name: siege1.name.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), value: siege1.time.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
+      { name: siege1.winner, value: siege1.balance }
+    )
+    .setTimestamp()
+    .setFooter({text: "Bot written by Shadowevil015", iconURL:"https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437"});
+
+    if (siegeList[1] != undefined) {
+      embedSiege.addFields(
+        { name: siege2.name.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), value: siege2.time.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
+        { name: siege2.winner, value: siege2.balance }
+      );
+    }
+    if (siegeList[2] != undefined) {
+      embedSiege.addFields(
+        { name: siege3.name.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), value: siege3.time.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
+        { name: siege3.winner, value: siege2.balance }
+      );
+    }
+    if (siegeList[3] != undefined) {
+      embedSiege.addFields(
+        { name: siege4.name.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), value: siege4.time.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
+        { name: siege4.winner, value: siege4.balance }
+      );
+    }
+    if (siegeList[4] != undefined) {
+      embedSiege.addFields(
+        { name: siege5.name.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), value: siege5.time.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
+        { name: siege5.winner, value: siege5.balance }
+      );
+    }
+    if (siegeList[5] != undefined) {
+      embedSiege.addFields(
+        { name: siege6.name.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), value: siege6.time.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
+        { name: siege6.winner, value: siege6.balance }
+      );
+    }
 
     if (siegeList.length === 0) {
       await interaction.reply("There are no sieges currently!");
     } else {
-      const siege = new MessageEmbed()
-        .setColor("#EE6123")
-        .setTitle(bold(`Current Sieges`))
-        .addFields(
-          { name: "\u200B", value: strsieges.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
-          { name: "\u200B", value: strtimes.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true },
-          { name: "\u200B", value: strbalances.replaceAll(/"|]|/g, "").replaceAll(/_/g, " ").replaceAll(/,/g, "\n\n").replace("[", ""), inline: true }
-        )
-        .setTimestamp()
-        .setFooter({text: "Bot written by Shadowevil015", iconURL:"https://minecraft-mp.com/images/favicon/204623.png?ts=1615034437"});
-
-      await interaction.reply({ embeds: [siege] });
+      await interaction.reply({ embeds: [embedSiege] });
     }
   },
 };
